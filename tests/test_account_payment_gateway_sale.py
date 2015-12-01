4f71702f15b95ea3b8952f34735bcedbba9ca07a
resteve <resteve@zikzakmedia.com>
2015-12-01 11:01:35 +0100

    Tests

--- tests/test_account_payment_gateway_sale.py
+++ tests/test_account_payment_gateway_sale.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part account_payment_gateway_sale module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_payment_gateway_sale module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountPaymentGatewaySaleTestCase(unittest.TestCase):
+class AccountPaymentGatewaySaleTestCase(ModuleTestCase):
     'Test Account Payment Gateway Sale module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_payment_gateway_sale')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_payment_gateway_sale')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_payment_gateway_sale'
 
 
 def suite():
