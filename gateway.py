c53b795886d222d33a24feea8a69116cc9562667
jmartin <jmartin@zikzakmedia.com>
2016-02-18 14:05:12 +0100

    Implements confirm method of gateway transaction

--- gateway.py
+++ gateway.py
@@ -1,7 +1,8 @@
 # This file is part account_payment_gateway_sale module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
+
 
 __all__ = ['AccountPaymentGatewayTransaction']
 __metaclass__ = PoolMeta
@@ -15,3 +16,25 @@ class AccountPaymentGatewayTransaction:
         res = super(AccountPaymentGatewayTransaction, cls)._get_origin()
         res.append('sale.sale')
         return res
+
+    @classmethod
+    def confirm(cls, transactions):
+        pool = Pool()
+        Sale = pool.get('sale.sale')
+
+        for transaction in transactions:
+            if isinstance(transaction.origin, Sale):
+                sale = transaction.origin
+                if not sale.reference:
+                    Sale.set_reference([sale])
+                sale.description = sale.reference
+                sale.save()
+                Sale.workflow_to_done([sale])
+
+                for invoice in sale.invoices:
+                    if invoice.total_amount == transaction.amount:
+                        break
+
+                setattr(transaction, 'origin', invoice)
+
+        super(AccountPaymentGatewayTransaction, cls).confirm(transactions)
